f96de26e8a5bcad757a2dafcdf0e9457f4234589
AronTech11 <145631191+AronTech11@users.noreply.github.com>
2024-12-13 15:33:56 -0500

    Update authApi.js

--- frontend/src/redux/features/auth/authApi.js
+++ frontend/src/redux/features/auth/authApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const authApi = createApi({
   reducerPath: "authApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://maram-blogs-backend.onrender.com/",
+    baseUrl: "https://maram-blogs-backend.onrender.com/api/auth",
     credentials: "include",
   }),
   endpoints: (builder) => ({
