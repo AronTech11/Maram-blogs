b8cc8a255f660a62ea34bd2b90ca8e81874a7e85
Aron Hopson <aronhopson@Aron-Hopson-2.local>
2026-08-06 15:05:42 -0400

    Add quick Edit Blog button on single blog view for admins

--- frontend/src/pages/blogs/singleBlog/SingleBlog.jsx
+++ frontend/src/pages/blogs/singleBlog/SingleBlog.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { useFetchBlogByIdQuery } from "../../../redux/features/blogs/blogsApi";
+import { useSelector } from "react-redux";
 import SingleBlogCard from "./SingleBlogCard";
 import CommentCard from "../comments/CommentCard";
 import RelatedBlogs from "./RelatedBlogs";
@@ -9,6 +10,8 @@ import BlogGallery from "../../../components/BlogGallery";
 const SingleBlog = () => {
   const { id } = useParams();
   const { data: blog = {}, error, isLoading } = useFetchBlogByIdQuery(id);
+  const { user } = useSelector((state) => state.auth);
+  const isAdminOrAbove = user?.role === "admin" || user?.role === "superadmin";
 
   return (
     <div className="pt-20 pb-16">
@@ -26,6 +29,23 @@ const SingleBlog = () => {
         {blog.post && (
           <div className="flex flex-col lg:flex-row gap-8">
             <div className="lg:w-2/3">
+
+              {/* Quick Edit button for admins */}
+              {isAdminOrAbove && (
+                <div className="flex justify-end mb-3">
+                  <Link
+                    to={`/dashboard/update-items/${id}`}
+                    className="inline-flex items-center gap-1.5 bg-accent hover:bg-accent/90 text-white text-sm font-medium px-4 py-2 rounded-lg transition"
+                  >
+                    <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                      <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/>
+                      <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"/>
+                    </svg>
+                    Edit Blog
+                  </Link>
+                </div>
+              )}
+
               <BlogGallery content={blog.post.content} mode="grid" />
               <SingleBlogCard blog={blog.post} />
 
